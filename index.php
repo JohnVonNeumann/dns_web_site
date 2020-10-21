5637ec84fbe9d7077f43c3d58913c6c1354afa60
JohnVonNeumann <louiswillcock@gmail.com>
2020-10-22 03:12:53 +1100

    UPDATE index.php - remove boilerplate PDO test code

--- index.php
+++ index.php
@@ -22,21 +22,4 @@
 
         </main>
 
-        <?php
-        phpinfo();
-
-        $servername = "127.0.0.1";
-        $username = "devuser";
-        $password = "devpass";
-
-        try {
-            $conn = new PDO("mysql:host=$servername:port=9906;dbname=test_db", $username, $password);
-            // set the PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected successfully";
-        } catch(PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-        }
-        ?>
-
 <?php include("footer.inc"); ?>
