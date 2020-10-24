7c0a910fe11046548134fd40cc02c786c4b9aea9
JohnVonNeumann <louiswillcock@gmail.com>
2020-10-25 01:50:38 +1100

    UPDATE manage.php - add new func for new backend call

--- manage.php
+++ manage.php
@@ -12,13 +12,25 @@
         xhttp.open("GET", "list_all_attempts.php", false);
         xhttp.send();
     }
+
+    function listAllFirstAttempts100Percent() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                // Action to be performed when the document is read;
+                document.getElementById("content").innerHTML = this.responseText;
+            }
+        };
+        xhttp.open("GET", "list_all_first_attempts_100_percent.php", false);
+        xhttp.send();
+    }
 </script>
 
 <section>
     <h3>Results</h3>
-    <form>
-        <button id="listallattempts" onclick="showUser()"> Hello </button>
-    </form>
+    <button id="listallattempts" onclick="listAllAttempts()"> List All Attempts </button>
+    <button id="listallfirstattempts100percent" onclick="listAllFirstAttempts100Percent()"> List All First Attempts with 100% </button>
+    <table id="content"></table>
 </section>
 
 <?php include("footer.inc"); ?>
