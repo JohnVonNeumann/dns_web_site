4d0136f3ae48910a8f503f25daca2c1196be0730
JohnVonNeumann <louiswillcock@gmail.com>
2020-10-25 03:00:25 +1100

    CREATE list_all_attempts_by_student_id.php - new backend call

--- manage.php
+++ manage.php
@@ -37,6 +37,18 @@
         xhttp.open("GET", "list_all_third_attempt_50_percent.php", false);
         xhttp.send();
     }
+
+    function listAllAttemptsByStudentID(student_id) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                // Action to be performed when the document is read;
+                document.getElementById("content").innerHTML = this.responseText;
+            }
+        };
+        xhttp.open("GET", "list_all_attempts_by_student_id.php?q="+student_id, false);
+        xhttp.send();
+    }
 </script>
 
 <section>
@@ -44,6 +56,11 @@
     <button id="listallattempts" onclick="listAllAttempts()"> List All Attempts </button>
     <button id="listallfirstattempts100percent" onclick="listAllFirstAttempts100Percent()"> List All First Attempts with 100% </button>
     <button id="listallthirdattempt50percent" onclick="listAllThirdAttemptLessThan50Percent()"> List All Third Attempts with less than 50% </button>
+    <p>
+        <label for="student_id"> Student ID: </label>
+        <input type="text" name="student_id" id="student_id" />
+        <button id="listattemptbystudentid" onclick="listAllAttemptsByStudentID(document.getElementById('student_id').value)"> All attempts by Student ID </button>
+    </p>
     <table id="content"></table>
 </section>
 
